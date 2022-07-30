91dbb3be01e741c18722360024b8e1e27bd9a376
Aigars Maskalans <aigars545@gmail.com>
2022-07-30 17:12:52 +0300

    exercise7 done

---
+++ csharp-basics/exercises/TypesAndVariables/Exercise7/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace Exercise7
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter a sentence");
+            string sentence = Console.ReadLine();
+
+            int uperrCaseCount = 0;
+
+            for (int i = 0; i < sentence.Length; i++)
+            {
+                if (char.IsUpper(sentence[i])) uperrCaseCount++;
+            }
+
+            Console.WriteLine(uperrCaseCount);
+        }
+    }
+}
