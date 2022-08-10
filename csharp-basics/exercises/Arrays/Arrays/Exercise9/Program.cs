922d18b4543baaec46196b6830412360b01dd9c4
Aigars Maskalans <aigars545@gmail.com>
2022-08-10 22:42:30 +0300

    exercise 9 fix

--- csharp-basics/exercises/Arrays/Arrays/Exercise9/Program.cs
+++ csharp-basics/exercises/Arrays/Arrays/Exercise9/Program.cs
@@ -6,25 +6,31 @@ namespace Exercise9
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Enter some words separated with spaces!");
-            var userInput = Console.ReadLine();
-            string[] arrayOfWords = userInput.Trim().Split(" ");
+            var arr1 = new string[] { "mavis", "senaida", "letty" };
+            var arr2 = new string[] { "samuel", "MABELLE", "letitia", "meridith" };
+            var arr3 = new string[] { "Slyvia", "Kristal", "Sharilyn", "Calista" };
 
-            var arrayOfWordsLowerCase = arrayOfWords.Select(s => s.ToLowerInvariant()).ToArray();
-            var firstCharUpper = "";
-            var firstCharUpperArray = new string[arrayOfWordsLowerCase.Length];
-
-            foreach (var word in arrayOfWordsLowerCase)
-            {
-                char[] charArray = word.ToCharArray();
-                charArray[0] = char.ToUpper(charArray[0]);
-                firstCharUpper += new string(charArray) + " ";
-            }
-            
-            firstCharUpperArray = firstCharUpper.Split(" ");
-
-            Console.WriteLine(String.Join(" ", firstCharUpperArray));
+            CapMe(arr1);
+            CapMe(arr2);
+            CapMe(arr3);
             Console.ReadKey();
+
+            static void CapMe(string[] arrayOfWords)
+            {
+                var arrayOfWordsLowerCase = arrayOfWords.Select(s => s.ToLowerInvariant()).ToArray();
+                var firstCharUpper = "";
+                var firstCharUpperArray = new string[arrayOfWordsLowerCase.Length];
+
+                foreach (var word in arrayOfWordsLowerCase)
+                {
+                    char[] charArray = word.ToCharArray();
+                    charArray[0] = char.ToUpper(charArray[0]);
+                    firstCharUpper += new string(charArray) + " ";
+                }
+                
+                firstCharUpperArray = firstCharUpper.Split(" ");
+                Console.WriteLine(String.Join(" ", firstCharUpperArray));
+            }
         }
     }
 }
