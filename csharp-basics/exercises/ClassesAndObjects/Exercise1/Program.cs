7391df6fb5a7132ef648c86ab5440b064a184935
Aigars Maskalans <aigars545@gmail.com>
2022-08-13 19:42:01 +0300

    exercise 1 re

--- csharp-basics/exercises/ClassesAndObjects/Exercise1/Program.cs
+++ csharp-basics/exercises/ClassesAndObjects/Exercise1/Program.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.Diagnostics;
+using System.Runtime.InteropServices;
 
 namespace Exercise1
 {
@@ -35,24 +37,65 @@ namespace Exercise1
     {
         static void Main(string[] args)
         {
-            Product p1 = new Product("Logitech mouse", 70.00, 14);
-            Product p2 = new Product("iPhone 5s", 999.99, 3 );
-            Product p3 = new Product("Epson EB-U05", 440.46, 1);
+            Product p1 = new Product("1. Logitech mouse", 70.00, 14);
+            Product p2 = new Product("2. iPhone 5s", 999.99, 3 );
+            Product p3 = new Product("3. Epson EB-U05", 440.46, 1);
            
             Console.WriteLine("Original \n");
             p1.PrintProduct();
             p2.PrintProduct();
             p3.PrintProduct();
 
-            p1.ChangeQuantity(1);
-            p2.ChangePrice(500.99);
+            Console.WriteLine("\nWhich product u would like to change\n");
+            int userChoice = Convert.ToInt32(Console.ReadLine());
 
-            Console.WriteLine("\nChanged \n");
+            switch (userChoice)
+            {
+                case 1:
+                    Change(p1);
+                    break;
+                case 2:
+                    Change(p2);
+                    break; 
+                case 3:
+                    Change(p3);
+                    break;
+                default:
+                    Console.WriteLine("Error");
+                    Console.ReadKey();
+                    Environment.Exit(0);
+                    break;
+            }
+
+            Console.Clear();
             p1.PrintProduct();
             p2.PrintProduct();
             p3.PrintProduct();
-
             Console.ReadKey();
+        }
+
+        private static void Change(Product p)
+        {
+            Console.WriteLine("\nWhat would u like to Change?");
+            Console.WriteLine("Press 1 for price");
+            Console.WriteLine("Press 2 for amount");
+            int userChoice = Convert.ToInt32(Console.ReadLine());
+            Console.Write("Enter new value: ");
+            var newValue = Convert.ToDouble(Console.ReadLine());
+
+            switch (userChoice)
+            {
+                case 1:
+                    p.ChangePrice(newValue);
+                    break;
+                case 2:
+                    p.ChangeQuantity((int)newValue);
+                    break;
+                default:
+                    Console.WriteLine("Error");
+                    Console.ReadKey();
+                    break;
+            }
         }
     }
 }
