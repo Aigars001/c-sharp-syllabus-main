8384a43bebb7d53cb3991b082a832b3e77c8e1a7
Aigars Maskalans <aigars545@gmail.com>
2022-08-14 17:54:35 +0300

    exercise 3 done

---
+++ csharp-basics/exercises/ClassesAndObjects/Exercise3/FuelGauge.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exercise3
+{
+    public class FuelGauge
+    {
+        private int _fuelLevel;
+
+        public int ReportFuelLevel()
+        {
+            return _fuelLevel;
+        }
+
+        public bool checkFuelLevel()
+        {
+            bool isEmpty = false;
+            if (_fuelLevel < 1)
+            {
+                isEmpty = true;
+                Console.WriteLine("You are out of fuel");
+            }
+            return isEmpty;
+        }
+
+        public void FillUp()
+        {
+            if (_fuelLevel < 70)
+            {
+                _fuelLevel++;
+            }
+        }
+
+        public void BurnFuel()
+        {
+            if (!checkFuelLevel())
+            {
+                _fuelLevel--;
+            }
+        }
+    }
+}
