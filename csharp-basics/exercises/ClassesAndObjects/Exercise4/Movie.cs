80a8f2a55330a2f4886f72e181db3599ce3671a3
Aigars Maskalans <aigars545@gmail.com>
2022-08-14 21:13:49 +0300

    exercise 4 done

---
+++ csharp-basics/exercises/ClassesAndObjects/Exercise4/Movie.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exercise4
+{
+    public class Movie
+    {
+        private string _title;
+        private string _studio;
+        private string _rating;
+
+        public Movie(string title, string studio, string rating)
+        {
+            _title = title;
+            _studio = studio;
+            _rating = rating;
+        }
+
+        public Movie(string title, string studio)
+        {
+            _title = title;
+            _studio = studio;
+            _rating = "PG";
+        }
+
+        public static List<Movie> GetPg(Movie[] arr)
+        {
+            List<Movie> pgArray = new List<Movie>();
+
+            foreach (var i in arr)
+            {
+                if (i._rating == "PG")
+                {
+                    pgArray.Add(i);
+                }
+            }
+            return pgArray;
+        }
+
+        public void PrintMovie()
+        {
+            Console.WriteLine($"{_title} from {_studio} is rated: {_rating}");
+        }
+    }
+}
