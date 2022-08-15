97cbc6c7c399ade87bc082183d9819af956c2ac8
Aigars Maskalans <aigars545@gmail.com>
2022-08-15 21:24:11 +0300

    Account done

--- csharp-basics/exercises/ClassesAndObjects/Account/Account.cs
+++ csharp-basics/exercises/ClassesAndObjects/Account/Account.cs
@@ -3,31 +3,32 @@
     class Account
     {
         private string _name;
-        private double _money;
+        private decimal _money;
 
-        public Account(string v1, double v2)
+        public Account(string v1, decimal v2)
         {
-            
+            _name = v1;
+            _money = v2;
         }
 
-        public double Withdrawal(double i)
+        public decimal Withdrawal(decimal i)
         {
-            return i;
+            return _money -=i;
         }
 
-        public void Deposit(double i)
+        public void Deposit(decimal i)
         {
-            
+            _money += i;
         }
 
-        public double Balance()
+        public decimal Balance()
         {
             return _money;
         }
 
         public override string ToString()
         {
-            return $"{_name}: {_money}";
+            return $"{_name}: {Balance()}";
         }
 
         public string Name
