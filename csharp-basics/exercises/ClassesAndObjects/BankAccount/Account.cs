03c2de9a9df9726643b9d8ce18e2d618ed4151b4
Aigars Maskalans <aigars545@gmail.com>
2022-08-15 18:09:55 +0300

    Bank account done

---
+++ csharp-basics/exercises/ClassesAndObjects/BankAccount/Account.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BankAccount
+{
+    class Account
+    {
+        public string _userAccountName;
+        public decimal _userAccountBalance;
+        public string _userAccountDetails;
+
+        public Account(string userAccountName, decimal userAccountBalance)
+        {
+            _userAccountName = userAccountName;
+            _userAccountBalance = userAccountBalance;
+        }
+
+        public string ShowUSerDetails()
+        {
+            var format = $"{Math.Abs(_userAccountBalance):0.00}";
+            if (_userAccountBalance < 0)
+            {
+                return _userAccountDetails = $"{_userAccountName}, -${format}";
+            }
+
+            return _userAccountDetails = $"{_userAccountName}, ${format}";
+        }
+    }
+}
--- csharp-basics/exercises/ClassesAndObjects/BankAccount/Program.cs
+++ csharp-basics/exercises/ClassesAndObjects/BankAccount/Program.cs
@@ -10,6 +10,16 @@ namespace BankAccount
     {
         static void Main(string[] args)
         {
+            Account acc1 = new Account("Benson", (decimal)17.25);
+            Account acc2 = new Account("Benson", (decimal)-17.25);
+            Account acc3 = new Account("Benson", (decimal)0);
+            Account acc4 = new Account("Benson", (decimal)200);
+
+            Console.WriteLine(acc1.ShowUSerDetails());
+            Console.WriteLine(acc2.ShowUSerDetails());
+            Console.WriteLine(acc3.ShowUSerDetails());
+            Console.WriteLine(acc4.ShowUSerDetails());
+            Console.ReadKey();
         }
     }
 }
