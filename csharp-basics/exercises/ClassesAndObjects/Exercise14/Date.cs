9c828cf859adc47d70d13298c1dbefef4e8a6994
Aigars Maskalans <aigars545@gmail.com>
2022-08-17 21:28:55 +0300

    Exercise14 done

---
+++ csharp-basics/exercises/ClassesAndObjects/Exercise14/Date.cs
@@ -0,0 +1,26 @@
+﻿using System.Globalization;
+
+namespace Exercise14
+{
+    internal class Date
+    {
+        private int _year;
+        private int _month;
+        private int _day;
+
+        public Date(int year, int month, int day)
+        {
+            _year = year;
+            _month = month;
+            _day = day;
+        }
+
+        public string WeekdayInDutch()
+        {
+            CultureInfo netherlands = CultureInfo.CreateSpecificCulture("nl-NL");
+            DateTime dateTime = new DateTime(_year, _month, _day);
+
+            return dateTime.ToString("dddd", netherlands);
+        }
+    }
+}
