84d64d86deb362a79b64ed99b7b8d2a3bd52f237
Aigars Maskalans <aigars545@gmail.com>
2022-08-17 11:50:01 +0300

    Exercise 12 ...done

---
+++ csharp-basics/exercises/ClassesAndObjects/Exercise12/Student.cs
@@ -0,0 +1,38 @@
+﻿namespace Exercise12
+{
+    public class Student : IStudent
+    {
+        private List<string> _testsTaken = new List<string>();
+
+        public List<string> TestsTaken
+        {
+            get
+            {
+                if (_testsTaken.Count == 0)
+                {
+                    Console.WriteLine("No tests taken");
+                }
+
+                return _testsTaken;
+            }
+        }
+
+        public void TakeTest(ITestPaper paper, string[] answers)
+        {
+            int countOfRightAnswers = paper.MarkScheme.Zip(answers,(i, j) => i == j).Count(eq => eq);
+            int percentComplete = (int)Math.Round((double)(100 * countOfRightAnswers) / answers.Length);
+
+            var markstr = paper.PassMark.Replace("%", "");
+            var mark = Int32.Parse(markstr);
+
+            if (mark <= percentComplete)
+            {
+                _testsTaken.Add($"{paper.Subject} Passed! ({percentComplete}%)");
+            }
+            else
+            {
+                _testsTaken.Add($"{paper.Subject} Failed! ({percentComplete}%)");
+            }
+        }
+    }
+}
