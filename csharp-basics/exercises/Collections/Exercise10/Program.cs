56d659b1fc8f3e3dd1633b4b7b1071ca291d62ee
Aigars Maskalans <aigars545@gmail.com>
2022-08-21 03:00:07 +0300

    Exercise 10 done

---
+++ csharp-basics/exercises/Collections/Exercise10/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace Exercise10
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var myset = new HashSet<string>();
+
+            myset.Add("aaaa");
+            myset.Add("bbbb");
+            myset.Add("cccc");
+            myset.Add("dddd");
+            myset.Add("eeee");
+
+            foreach (var str in myset)
+            {
+                Console.WriteLine(str);
+            }
+
+            myset.Clear();
+
+            Console.WriteLine(CheckForDuplicate(myset));
+            Console.ReadKey();
+        }
+
+        public static bool CheckForDuplicate(HashSet<string> set)
+        {
+            return set.Count != set.Distinct().Count();
+        }
+    }
+}
