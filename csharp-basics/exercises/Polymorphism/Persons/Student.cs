9d28114cab7acb5f5e119e185228fc99ee6a8697
Aigars Maskalans <aigars545@gmail.com>
2022-08-24 17:03:00 +0300

    persons done

---
+++ csharp-basics/exercises/Polymorphism/Persons/Student.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Persons
+{
+    public class Student : Person
+    {
+        public double Gpa { get; }
+
+        public Student(string name, string lastName, string address, int id, double gpa) 
+            : base(name, lastName, address, id)
+        {
+            Gpa = gpa;
+        }
+
+        public override void Display()
+        {
+            Console.WriteLine($"{FirstName} {LastName}, from {Address}, GPA: {Gpa}. ID: {Id}");
+        }
+    }
+}
