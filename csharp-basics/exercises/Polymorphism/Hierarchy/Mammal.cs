a01dce78cf4268cdadd49b76b86e88f7cceff25f
Aigars Maskalans <aigars545@gmail.com>
2022-08-27 17:03:27 +0300

    hierarchy done

---
+++ csharp-basics/exercises/Polymorphism/Hierarchy/Mammal.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Hierarchy
+{
+    public abstract class Mammal : Animal
+    {
+        public string LivingRegion { get; }
+
+        protected Mammal(string animalName, string animalType, double animalWeight, string livingRegion)
+            : base(animalName, animalType, animalWeight)
+        {
+            LivingRegion = livingRegion;
+        }
+
+        public override void AnimalInfo()
+        {
+            Console.WriteLine($"{AnimalType} {AnimalName} {AnimalWeight} {LivingRegion}");
+        }
+
+        public override string ToString()
+        {
+            return $"{AnimalType} {AnimalName} Weight: {AnimalWeight:F2} lives at: {LivingRegion} ate {FoodEaten} food\n";
+        }
+    }
+}
--- csharp-basics/exercises/Polymorphism/Hierarchy/Program.cs
+++ csharp-basics/exercises/Polymorphism/Hierarchy/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 
 namespace Hierarchy
 {
@@ -6,7 +7,28 @@ namespace Hierarchy
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            var cat = new Cat("Kitty", "grey", 1.1, "Home", "Persian");
+            var tiger = new Tiger("Typcho", "Tiger", 167.5, "Asia");
+            var mouse = new Mouse("steward", "Mouse", 0.25444, "Home");
+            var zebra = new Zebra("Zeee", "Zebra", 30, "Africa");
+            var animals = new List<Animal>
+            {
+                cat, mouse, tiger, zebra,
+            };
+
+            var veg = new Vegetable("Vegetable", 1);
+            var meat = new Meat("Meat", 1);
+
+            foreach (var animal in animals)
+            {
+                animal.AnimalInfo();
+                animal.MakeSound();
+                animal.Eat(veg);
+                animal.Eat(meat);
+                Console.WriteLine(animal);
+            }
+
+            Console.ReadKey();
         }
     }
 }
