2c66d65aa487f5154f419b83ccd3035494b189af
Aigars Maskalans <aigars545@gmail.com>
2022-08-29 22:12:04 +0300

    Exercise 1 fix

--- csharp-basics/exercises/ClassesAndObjects/Exercise1/Program.cs
+++ csharp-basics/exercises/ClassesAndObjects/Exercise1/Program.cs
@@ -4,35 +4,6 @@ using System.Runtime.InteropServices;
 
 namespace Exercise1
 {
-    internal class Product
-    {
-        private string _name;
-        private double _priceAtStart;
-        private int _amountAtStart;
-
-        public Product(string name, double priceAtStart, int amountAtStart)
-        {
-            _name = name;
-            _priceAtStart = priceAtStart;
-            _amountAtStart = amountAtStart;
-        }
-
-        public void PrintProduct()
-        {
-            Console.WriteLine($"{_name} price: {_priceAtStart} EUR, amount: {_amountAtStart} units");
-        }
-
-        public int ChangeQuantity(int newAmount)
-        {
-            return _amountAtStart = newAmount;
-        }
-
-        public double ChangePrice(double newPrice)
-        {
-            return _priceAtStart = newPrice;
-        }
-    }
-
     internal class Program
     {
         static void Main(string[] args)
